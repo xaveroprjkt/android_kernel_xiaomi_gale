cd274a1f55e74ebd7e4d68fb0d3a68abe8c2b71d
Vlad Dumitrescu <vladum@google.com>
2018-11-22 14:39:16 -0500

    UPSTREAM: bpf: add skb->tstamp r/w access from tc clsact and cg skb progs
    This could be used to rate limit egress traffic in concert with a qdisc
    which supports Earliest Departure Time, such as FQ.
    
    Write access from cg skb progs only with CAP_SYS_ADMIN, since the value
    will be used by downstream qdiscs. It might make sense to relax this.
    
    Changes v1 -> v2:
      - allow access from cg skb, write only with CAP_SYS_ADMIN
    
    Change-Id: I5e4e521bca04b4b402bfd7853cf11cbc285b6067
    Signed-off-by: Vlad Dumitrescu <vladum@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -2701,6 +2701,7 @@ struct __sk_buff {
 	__u32 data_meta;
 	__bpf_md_ptr(struct bpf_sock *, sk);
 	struct bpf_flow_keys *flow_keys;
+	__u64 tstamp;
 };
 
 struct bpf_tunnel_key {
--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -2410,6 +2410,8 @@ struct __sk_buff {
 	/* ... here. */
 
 	__u32 data_meta;
+	struct bpf_flow_keys *flow_keys;
+	__u64 tstamp;
 };
 
 struct bpf_tunnel_key {
