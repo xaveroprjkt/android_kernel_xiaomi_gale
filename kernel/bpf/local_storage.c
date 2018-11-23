ab50c08b76f36d5dbae7225110489d5b83900c34
Rustam Kovhaev <rkovhaev@gmail.com>
2018-11-23 15:48:16 -0800

    UPSTREAM: bpf, tags: Fix DEFINE_PER_CPU expansion
    Building tags produces warning:
    
      ctags: Warning: kernel/bpf/local_storage.c:10: null expansion of name pattern "\1"
    
    Let's use the same fix as in commit 25528213fe9f ("tags: Fix DEFINE_PER_CPU
    expansions"), even though it violates the usual code style.
    
    Change-Id: I16fb10701f474e04fdc9bf1856c691be05b42fbc
    Signed-off-by: Rustam Kovhaev <rkovhaev@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- kernel/bpf/local_storage.c
+++ kernel/bpf/local_storage.c
@@ -9,8 +9,7 @@
 #include <linux/slab.h>
 #include <uapi/linux/btf.h>
 
-DEFINE_PER_CPU(struct bpf_cgroup_storage*,
-	       bpf_cgroup_storage[MAX_BPF_CGROUP_STORAGE_TYPE]);
+DEFINE_PER_CPU(struct bpf_cgroup_storage*, bpf_cgroup_storage[MAX_BPF_CGROUP_STORAGE_TYPE]);
 
 #ifdef CONFIG_CGROUP_BPF
 
