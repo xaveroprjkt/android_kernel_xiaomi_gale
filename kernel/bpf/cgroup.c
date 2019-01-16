bb1450ef729de5718fd77c640885c930b526721e
Mathieu Malaterre <malat@debian.org>
2019-01-16 20:35:29 +0100

    UPSTREAM: bpf: Annotate implicit fall through in cgroup_dev_func_proto
    There is a plan to build the kernel with -Wimplicit-fallthrough
    and this place in the code produced a warnings (W=1).
    
    This commit removes the following warning:
    
      kernel/bpf/cgroup.c:719:6: warning: this statement may fall through [-Wimplicit-fallthrough=]
    
    Change-Id: Icb19e4815c8887e95dea5897cdcc7468680466dc
    Signed-off-by: Mathieu Malaterre <malat@debian.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- kernel/bpf/cgroup.c
+++ kernel/bpf/cgroup.c
@@ -725,6 +725,7 @@ cgroup_base_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 	case BPF_FUNC_trace_printk:
 		if (capable(CAP_SYS_ADMIN))
 			return bpf_get_trace_printk_proto();
+		/* fall through */
 	default:
 		return NULL;
 	}
